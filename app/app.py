666036a6b3c93cecc84910d803556e44e6eddf12
dhirajforyou <govind.nitk@gmail.com>
2023-09-30 16:34:31 +0530

    added templates and serving with bootstrap

--- app/app.py
+++ app/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .routes import routes
+
+
+def register_extensions(app):
+    bootstrap = Bootstrap()
+    bootstrap.init_app(app)
 
 
 def create_app():
     app = Flask(__name__)
+    # app configuration
+    # serve bootstrap from local. Later in production environment, take from cdn.
+    app.config["BOOTSTRAP_SERVE_LOCAL"] = True
+    register_extensions(app)
     app.register_blueprint(routes)
     return app
+
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
-from flask import Blueprint
+from flask import render_template, Blueprint
 
 routes = Blueprint('routes', __name__)
 
 
 @routes.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('base.html')
 
 
+@routes.route('/user/')
 @routes.route('/user/<name>')
-def user(name):
-    return f"<h1>Hello, {name}</h1>"
+def user(name=None):
+    return render_template('user.html', name=name)
