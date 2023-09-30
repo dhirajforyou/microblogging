e6c1c6fd196c323b46b1bee15ab480afdf24fb05
dhirajforyou <govind.nitk@gmail.com>
2023-09-30 14:17:00 +0530

    added new route to greet user

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def index():
     return "Hello, world!"
 
 
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Hello, {name}</h1>"
+
+
 def main():
     app.run(host="0.0.0.0", port=6001, debug=True)
 
